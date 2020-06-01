fda456f525a8f806159d7a2600fad33cd8887efd
Alejo Gschwind <alejogschwind.97@gmail.com>
2020-06-01 19:45:41 -0300

    Updates

---
+++ precache-manifest.719dc577b38aa07bdcb155dcd23ce21e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf725ea6f3f318bc5792f352a3ec10d",
+    "url": "/FlagApp/index.html"
+  },
+  {
+    "revision": "e3f91d895247f280cf64",
+    "url": "/FlagApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "84f72829084943a19737",
+    "url": "/FlagApp/static/js/2.2931f975.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/FlagApp/static/js/2.2931f975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3f91d895247f280cf64",
+    "url": "/FlagApp/static/js/main.78d14e05.chunk.js"
+  },
+  {
+    "revision": "96b42816f39bf5091f28",
+    "url": "/FlagApp/static/js/runtime-main.d2980795.js"
+  },
+  {
+    "revision": "39b498bfd0c2a411a748ab620fedbc8b",
+    "url": "/FlagApp/static/media/moon-dark.39b498bf.svg"
+  },
+  {
+    "revision": "7dd9cc7177f8de3eeb105b8a498f1248",
+    "url": "/FlagApp/static/media/moon-light.7dd9cc71.svg"
+  }
+]);
